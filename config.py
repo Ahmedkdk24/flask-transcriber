7ec849c859dadd13fda94cb565b6c1ff826e0603
Ahmed Ibrahim <ahmedkdk11@gmail.com>
2025-08-18 17:42:07 +0300

    Removed deleted files and folders

--- config.py
+++
@@ -1,7 +0,0 @@
-PROJECT = "supple-hulling-468516-g9"
-GCS_BUCKET = "gcs_bucket-1"
-
-GPU_INSTANCE_NAME = "flask-transcribe-vm-instance"
-GPU_INSTANCE_ZONE = "asia-south1-b"  # or your actual zone
-
-POLL_INTERVAL = 10  # seconds between bucket checks
